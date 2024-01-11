46518845a45d97ccc9e0397f2e10289bf94193ac
Sk Hashim Ali <hasem1997@gmail.com>
2024-01-12 02:34:32 +0530

    ordered product list and showed  amount

--- src/Page Component/Products/Products.jsx
+++ src/Page Component/Products/Products.jsx
@@ -164,7 +164,7 @@ export default function Products() {
         headerItems={headerItems}
         rowURL="/productItem/"
         filterItems={filterItems}
-        thData={["Name", "Category","Brand","Stock", "Min Stock","Max Stock","MRP","Unit"]}
+        thData={["Name", "Category","Brand","Stock", "Min Stock","Max Stock","MRP","Amount"]}
         trData={productList}
         dispatch={dispatch}
         pageNo={pageNo}
